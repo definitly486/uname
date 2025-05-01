786a018dffba41316b8e5c0e0f6a580387484b91
Your Name <you@example.com>
2025-05-01 12:39:59 +0300

    новый файл:    uname 	новый файл:    uname.sh

---
+++ uname.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+item=$(uname -a)
+my_variable=$(grep -r "$(uname -a | awk '{print $6}' )"  uname)
+
+       if [ -z "$my_variable" ]; then
+  echo "my_variable is null."
+  echo $item >> uname 
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add --all
+git add .
+git commit -n
+git pull https://github.com/definitly486/uname.git
+           else
+  echo "my_variable is non-null."
+  
+
+fi
